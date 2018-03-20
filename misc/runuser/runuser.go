02ee4c10069233b853fe7ece406273743383e0d2
nnnn20430 <nnnn20430@mindcraft.si.eu.org>
2018-03-20 20:52:37 +0100

    misc/runuser: made it more usable
    Now it can use usernames instead of user id's and same with groups,
    also it now sets up some basic env variables too, and it looks up
    command paths.

--- misc/runuser/runuser.go
+++ misc/runuser/runuser.go
@@ -5,30 +5,89 @@ import (
 	"flag"
 	"os"
 	"os/user"
+	"os/exec"
 	"syscall"
 	"strings"
 	"strconv"
 )
 
+var (
+	err error;
+	env = os.Environ();
+
+	f_setsid bool;
+	f_preserve_env bool;
+	f_login bool;
+)
+
+func parseArgs() {
+	flag.Usage = func() {
+		fmt.Printf("%v: [options] user[:group] command [args...]\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	flag.BoolVar(&f_setsid, "s", false, "Create new session (setsid)")
+	flag.BoolVar(&f_preserve_env, "p", false, "Preserve environment")
+	flag.BoolVar(&f_login, "l", false, "Run the command as a login shell, envrionment is cleared and \"-p\" flag is ignored")
+	flag.Parse()
+	if f_login {
+		f_preserve_env = false
+	}
+	if len(flag.Args()) < 2 {
+		flag.Usage()
+	}
+}
+
+func envDel(key string) {
+	var envNew = []string{};
+	for _, e := range env {
+		pair := strings.Split(e, "=")
+		if pair[0] != key {
+			envNew = append(envNew, e)
+		}
+	}
+	env = envNew
+}
+
+func envGet(key string) (string, bool) {
+	for _, e := range env {
+		pair := strings.Split(e, "=")
+		if pair[0] == key && len(pair) > 1 {
+			return pair[1], true
+		}
+	}
+	return "", false
+}
+
+func envSet(key, val string) {
+	envDel(key)
+	env = append(env, strings.Join([]string{key, val}, "="))
+}
+
 func main() {
 	var (
-		setsid bool;
 		strArr []string;
 		uid uint32;
 		gid uint32;
 		groups []uint32;
 		pArgs []string;
+		pCmd string;
 		p *os.Process;
-		err error;
 	)
-	flag.BoolVar(&setsid, "s", false, "create new session (setsid)")
-	flag.Parse()
-	if len(flag.Args()) < 2 {
-		fmt.Printf("%v: [-s] uid[:gid] command [args]\n", os.Args[0])
-		os.Exit(1)
+	parseArgs()
+	if f_login {
+		var v, ok = envGet("TERM")
+		env = []string{}
+		if ok {
+			envSet("TERM", v)
+		}
+		envSet("PATH", "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin")
 	}
 	strArr = strings.Split(flag.Args()[0], ":")
 	if i, err := strconv.Atoi(strArr[0]); err == nil {
+		uid = uint32(i)
+	} else if u, err := user.Lookup(strArr[0]); err == nil {
+		i, _ = strconv.Atoi(u.Uid)
 		uid = uint32(i)
 	} else {
 		fmt.Printf("%v\n", err)
@@ -37,6 +96,9 @@ func main() {
 	if len(strArr) > 1 && strArr[1] != "" {
 		if i, err := strconv.Atoi(strArr[1]); err == nil {
 			gid = uint32(i)
+		} else if g, err := user.LookupGroup(strArr[1]); err == nil {
+			i, _ = strconv.Atoi(g.Gid)
+			gid = uint32(i)
 		} else {
 			fmt.Printf("%v\n", err)
 			os.Exit(1)
@@ -44,7 +106,7 @@ func main() {
 	} else {
 		gid = uid
 	}
-	if u, err := user.LookupId(strArr[0]); err == nil {
+	if u, err := user.LookupId(strconv.FormatUint(uint64(uid), 10)); err == nil {
 		if ug, err := u.GroupIds(); err == nil {
 			for _, v := range ug {
 				if i, err := strconv.Atoi(v); err == nil {
@@ -52,10 +114,23 @@ func main() {
 				}
 			}
 		}
+		if !f_preserve_env {
+			envSet("HOME", u.HomeDir)
+			envSet("USER", u.Username)
+		}
 	}
 	pArgs = make([]string, len(flag.Args()[1:]))
 	copy(pArgs, flag.Args()[1:])
-	p, err = os.StartProcess(pArgs[0], pArgs, &os.ProcAttr{
+	pCmd, err = exec.LookPath(pArgs[0])
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	if f_login {
+		pArgs[0] = "-"
+	}
+	p, err = os.StartProcess(pCmd, pArgs, &os.ProcAttr{
+		Env: env,
 		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
 		Sys: &syscall.SysProcAttr{
 			Credential: &syscall.Credential{
@@ -63,7 +138,7 @@ func main() {
 				Gid: gid,
 				Groups: groups,
 			},
-			Setsid: setsid,
+			Setsid: f_setsid,
 		},
 	})
 	if err != nil {
